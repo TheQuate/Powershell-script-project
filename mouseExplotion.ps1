28eb0c63a04fbb10a292b566f3ff1962d5c93263
TheQuate <123550040+TheQuate@users.noreply.github.com>
2024-05-06 09:46:09 +0200

    shit happens

--- mouseExplotion.ps1
+++ mouseExplotion.ps1
@@ -1,4 +1,4 @@
 while ($true) {
-    [System.Windows.Forms.Cursor]::Postion = New-Object System.Drawing.Point -ArgumentList (Get-Randm -Minimum 0 -Maximum 1920), (Get-Random -Minimum 0 -Maximum 1080)
+    [System.Windows.Forms.Cursor]::Postion = New-Object System.Drawing.Point -ArgumentList (Get-Random -Minimum 0 -Maximum 1920), (Get-Random -Minimum 0 -Maximum 1080)
     Start-Sleep -Milliseconds 100
 }
