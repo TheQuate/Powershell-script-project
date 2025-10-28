2ccf49194551b175e6b6763a955c5df5770fb79d
JST2006 <123550040+TheQuate@users.noreply.github.com>
2025-10-28 10:47:49 +0100

    Add files via upload
    New script for user generation in AD

---
+++ brukeroppretting.ps1
@@ -0,0 +1,26 @@
+$Users = Import-Csv -Path ".\csv_fil.csv"
+if ($Users.length -gt 0){
+    Write-Host "CSV import completed successfully and contains data."
+} else{
+    "CSV import completed, but no data rows were found (only headers or empty file)."
+}
+
+ 
+foreach ($User in $Users)            
+{            
+    $Displayname = $User.Firstname + " " + $User.Lastname            
+    $UserFirstname = $User.Firstname            
+    $UserLastname = $User.Lastname            
+    $OU = "$User.OU"            
+    $SAM = $User.SAM            
+    $UPN = $User.Firstname + "." + $User.Lastname + "@" + $User.Maildomain            
+   $Description = $User.Description            
+    $Password = $User.Password            
+try {
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server domain.loc            
+    Write-Host "User created"
+}
+catch {
+    Write-Host "User not Created"
+}   
+} 
